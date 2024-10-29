43f2ef4417e0f83f43f1cf956a55966ea6bd5b39
JCPlamo <justincplamo@gmail.com>
2024-10-29 16:06:58 -0500

    Add events to create a hover effect on a square

--- script.js
+++ script.js
@@ -5,8 +5,35 @@ function createDivs(num) {
     {
         const content = document.createElement("div");
         content.classList.add("square");
+
+        //Hover effect
+        content.addEventListener('mouseenter', () => {
+            content.style.backgroundColor = "black"; // Change background color on hover
+        });
+
+        content.addEventListener('mouseleave', () => {
+            //content.style.backgroundColor = ""; // Reset background color on leave
+        });
+
         container.appendChild(content);
+        
     }
 }
 
 createDivs(16*16); // Create 256 squares for a 16x16 grid
+
+
+// //Hover effect
+// const square = document.querySelector("div");
+
+// square.addEventListener('mouseenter', (event) => {
+//     // Code to execute when the mouse enters the element
+//     //square.body.style.backgroundColor = black;
+//     console.log("hover");
+//     square.body.style.backgroundColor = "black";
+// });
+
+// square.addEventListener('mouseleave', (event) => {
+//     // Code to execute when the mouse leaves the element
+//     console.log("leave");
+// });
