3d45d5f8e6afce6c11a2f315fe67b4ffde68196a
JCPlamo <justincplamo@gmail.com>
2024-10-29 15:31:32 -0500

    Add flexbox styling to .css to create grids

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const container = document.querySelector("#container");
 
-function createDivs(num)
-{
-    while(num > 0)
+function createDivs(num) {
+    for (let i = 0; i < num; i++) 
     {
         const content = document.createElement("div");
         content.classList.add("square");
         container.appendChild(content);
-        num--;
     }
 }
 
-
-createDivs(16);
+createDivs(16*16); // Create 256 squares for a 16x16 grid
